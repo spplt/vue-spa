519153e2dde23f9824894f395e58a07118015d24
spplt <julian@sappelt.net>
2017-12-01 15:41:34 +0200

    test
    test

--- vue-spa/src/assets/components/Post.vue
+++
@@ -1,7 +0,0 @@
-<template lang="html">
-  <div class="post" v-if="post">
-    <h1 class="post__title">{{ post.title }}</h1>
-    <p class="post__body">{{ post.body }}</p>
-    <p class="post__id">{{ post.id }}</p>
-  </div>
-</template>
